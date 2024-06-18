73d0d84620e5cb71fc4df4b1c2f14864ae27b49f
Alberto <berto.olmo@gmail.com>
2024-06-19 00:47:19 +0200

    [DEV] pause sistem improoved

--- Transcriber/main3.py
+++ Transcriber/main3.py
@@ -11,7 +11,7 @@ def check_ffmpeg():
         raise EnvironmentError("ffmpeg and/or ffprobe are not installed. Please install them to proceed.")
 
 
-def split_audio_by_silence(audio_path, min_silence_len=1400, silence_thresh=-40, keep_silence=500):
+def split_audio_by_silence(audio_path, min_silence_len=1320, silence_thresh=-40, keep_silence=500):
     audio = AudioSegment.from_mp3(audio_path)
     segments = silence.split_on_silence(audio,
                                         min_silence_len=min_silence_len,
@@ -43,7 +43,7 @@ def save_as_docx(text, output_path):
 check_ffmpeg()
 
 # Ruta objetivo al archivo mp3
-audio_file_path = 'test3.mp3'
+audio_file_path = 'testFinal.mp3'
 docx_file_path = os.path.splitext(audio_file_path)[0] + ".docx"
 
 # Dividir el audio en segmentos basados en pausas de al menos 1 segundo
@@ -66,9 +66,11 @@ punctuated_transcription = add_punctuation(full_transcription)
 
 # Reemplazar las etiquetas de pausa por saltos de línea dobles
 formatted_transcription = punctuated_transcription.replace(" <PAUSE>.", "\n\n")
+formatted_transcription2 = formatted_transcription.replace(" <PAUSE> ", " ")
+formatted_transcription3 = formatted_transcription2.replace(" <PAUSE>,", ",")
 
 # Guardar la transcripción como un documento DOCX
-save_as_docx(formatted_transcription, docx_file_path)
+save_as_docx(formatted_transcription3, docx_file_path)
 
-print(formatted_transcription)
+print(formatted_transcription3)
 print("Transcription saved to:", docx_file_path)
